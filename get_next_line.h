6bd19b5bb0b76eff795dba2661e8d30dd3d18c4f
Vincent MUCCHIELLI <vincent@Vincents-MacBook-Pro.local>
2018-10-12 13:12:55 +0200

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vimucchi <vimucchi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/11 21:27:48 by vimucchi          #+#    #+#             */
+/*   Updated: 2018/10/11 21:31:27 by vimucchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFFER	1
+
+int get_next_line(const int fd, char **line);
+
+#endif
