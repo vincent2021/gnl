c3120f670a64e5980bb3329d1d1aa47ed46fa7eb
Vincent MUCCHIELLI <vimucchi@student.42.fr>
2018-10-24 18:08:38 +0200

    mercredi 24

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,15 @@
 /*   By: vimucchi <vimucchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/19 12:22:15 by vimucchi          #+#    #+#             */
-/*   Updated: 2018/10/19 17:17:29 by vimucchi         ###   ########.fr       */
+/*   Updated: 2018/10/24 18:06:35 by vimucchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_list	*ft_list_mgmt(t_list *gnl, const int fd, char *str)
+t_list			*ft_list_mgmt(t_list *gnl, const int fd, char *str)
 {
-	t_list	*tmp;
+	t_list		*tmp;
 
 	tmp = gnl;
 	while (tmp)
@@ -28,44 +28,68 @@ t_list	*ft_list_mgmt(t_list *gnl, const int fd, char *str)
 	return (tmp);
 }
 
-int	get_next_line(const int fd, char **line)
+char			*ft_split(char *str)
 {
-	char			*buf;
-	char			*str;
-	static char		*remain;
+	int			i;
+
+	i = 0;
+	while (str[i] != '\n' && str[i])
+		i++;
+	if (str[i] == '\n' && str[i + 1] != '\0')
+	{
+		str = str + i + 1;
+		return (ft_strdup(str));
+	}
+	return (ft_strnew(1));
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	char		*buf;
+	char		*str;
+	static char	*remain;
 	int			i;
 	int			c_read;
 
-	str = ft_strnew(1000);
-	if (!remain)
-		remain = ft_strnew(100);
-	*line = str;	
-	if (!(buf = malloc(sizeof(char)*(BUFF_SIZE + 1))))
+	if (!(buf = malloc(sizeof(char) * (BUFF_SIZE + 1))))
 		return (-1);
 	if (line == NULL || read(fd, buf, 0) == -1 || fd < 0)
 		return (-1);
-	while((c_read = read(fd, buf, BUFF_SIZE)) != 0)
+	str = ft_strnew(1000);
+	*line = str;
+	if (!remain)
+		remain = ft_strnew(100);
+	else
+	{
+		i = 0;
+		while (remain[i] != '\n' && remain[i])
+			i++;
+		if (remain[i] == '\n' && i < (int)ft_strlen(remain))
+		{
+			ft_strncat(str, remain, i);
+			remain = remain + i + 1;
+			return (1);
+		}
+		else
+			ft_strcat(str, remain);
+	}
+	while ((c_read = read(fd, buf, BUFF_SIZE)) != 0)
 	{
 		i = 0;
 		buf[BUFF_SIZE + 1] = '\0';
-		while ((buf[i] != '\n' || buf[i] == EOF) && buf[i])			
+		while ((buf[i] != '\n' || buf[i] == EOF) && buf[i])
 			i++;
-		if (ft_strlen(remain) > 0)
-			ft_strncat(str, remain, ft_strlen(remain));
 		ft_strncat(str, buf, i);
-		if (buf[i] == '\n')
+		if (buf[i] == '\n' && i < BUFF_SIZE)
 		{
-			str = ft_strncpy(remain, buf + i + 1, BUFF_SIZE - i);
+			ft_strncpy(remain, buf + i + 1, BUFF_SIZE - i);
 			return (1);
 		}
 	}
-	if (c_read == 0 && ft_strlen(remain) == 0)
-		return (0);
-	else if (c_read == 0 && ft_strlen(remain) > 0)
+	if (c_read == 0)
 	{
-		ft_strncat(str, remain, ft_strlen(remain) - 1);
-		remain[0] = '\0';
-		return (1);
+		str[0] = '\0';
+		return (0);
 	}
 	return (-1);
 }
--- main.c
+++ main.c
@@ -6,27 +6,31 @@
 /*   By: vimucchi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/12 16:40:11 by vimucchi          #+#    #+#             */
-/*   Updated: 2018/10/19 17:07:30 by vimucchi         ###   ########.fr       */
+/*   Updated: 2018/10/24 17:49:09 by vimucchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		main(int argc, char **argv)
+int			main(int argc, char **argv)
 {
-	char *line;
+	char	*line;
 	int		fd;
+	int		i;
 
-	if(argc != 2)
+	if (argc != 2)
 		fd = 0;
 	else
 		fd = open(argv[1], O_RDONLY);
+	i = 1;
 	while (get_next_line(fd, &line) == 1)
 	{
-		ft_putendl(line);
+		ft_putnbr(i++);
+		ft_putstr(".");
+		ft_putstr(line);
 		free(line);
 	}
 	if (argc == 2)
 		close(fd);
-	return(0);
+	return (0);
 }
