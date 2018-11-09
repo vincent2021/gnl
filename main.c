671614d2eda878d6c789fad299114766642f8466
Vincent MUCCHIELLI <vincent@Vincents-MacBook-Pro.local>
2018-11-09 10:00:52 +0100

    change in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vimucchi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/12 16:40:11 by vimucchi          #+#    #+#             */
-/*   Updated: 2018/11/08 14:47:16 by vimucchi         ###   ########.fr       */
+/*   Updated: 2018/11/09 10:00:23 by vimucchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,13 @@ int			main(int argc, char **argv)
 {
 	char	*line;
 	int		fd;
-	int		i;
-	int		r;
 
 	if (argc != 2)
 		fd = 0;
 	else
 		fd = open(argv[1], O_RDONLY);
-	i = 0;
-	while ((r = get_next_line(fd, &line)) == 1)
+	while  (get_next_line(fd, &line) == 1)
 	{
-		ft_putstr("Return ");
-		ft_putnbr(r);
-		ft_putstr(" - Line ");
-		ft_putnbr(i);
-		ft_putchar('\n');
-		i++;
 		ft_putendl(line);
 		free(line);
 	}
